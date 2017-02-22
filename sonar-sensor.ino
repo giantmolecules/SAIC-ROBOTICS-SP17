643edc94dd7682e5f9974291c77d4a4e130042fe
Brett Ian Balogh <giantmolecules@gmail.com>
2017-02-22 15:42:30 -0600

    Update sonar-sensor.ino

--- sonar-sensor.ino
+++ sonar-sensor.ino
@@ -6,3 +6,13 @@ Connect sonar sensor to pin A1
 Prints the analog values from the sonar sensor to the terminal
 */
 
+int analogVal = 0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  analogVal = analogRead(1);
+  Serial.println(analogVal);
+}
